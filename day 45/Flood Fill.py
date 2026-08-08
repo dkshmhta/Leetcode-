7b2a6bbbd73e8014cb2a85e8c335d6cb1cff3579
daxx <daxx@gmail.com>
2026-08-08 14:38:23 +0530

    Solved Flood Fill

---
+++ day 45/Flood Fill.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def floodFill(self, image, sr, sc, color):
+        row = len(image)
+        col = len(image[0])
+        original = image[sr][sc]
+
+        if original == color:
+            return image
+
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= row or c >= col:
+                return
+
+            if image[r][c] != original:
+                return
+
+            image[r][c] = color
+
+            dfs(r - 1, c)
+            dfs(r + 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        dfs(sr, sc)
+        return image
